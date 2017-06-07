5af63608fedb205bfb90fd0af457eb27b64dab63
Bill Proudfoot <user@CODECLAN031.local>
2017-06-07 15:35:55 +0100

    deleted commented out code

--- client/components/TictactoeSquare.jsx
+++ client/components/TictactoeSquare.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 class TictactoeSquare extends React.Component {
 
   constructor(props) {
-    super(props)
-    // this.state={ buttonSymbol: this.props.position}
+    super(props)  
   }
 
 
@@ -14,32 +13,14 @@ class TictactoeSquare extends React.Component {
       <button onClick={ () => { this.handleClick() } }>{this.props.squareValue}</button>
       </div>
       );
-
-    //      <button onClick={ () => { this.props.handleClick(this.props.buttonSymbol) } }>{this.state.buttonSymbol}</button>
   }
 
   handleClick () {
-  // console.log("HandleClick", this.props.position)
+    if (this.props.squareValue !== "X" && this.props.squareValue !== "O" && this.props.winner === "Not Yet"){
 
-  if (this.props.squareValue !== "X" && this.props.squareValue !== "O" && this.props.winner === "Not Yet"){
-
-    this.props.squareClicked(this.props.position);
-    // console.log("HandleClick", this.props.position)
-
-
-    // if (this.props.crossesTurn) {
-    //   this.setState({buttonSymbol: "X"})
-    // }
-
-    // else {
-    //   this.setState({buttonSymbol: "O"})
-    // }
-    // this.props.recordMove(this.props.position);
-    // this.props.changeTurn();
-
-    // console.log(this.onClick)
+      this.props.squareClicked(this.props.position);
+    }
   }
-}
 
 
 }
