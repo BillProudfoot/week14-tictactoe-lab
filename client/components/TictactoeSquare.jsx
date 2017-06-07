c0ba8ec131332ed4b15746d7a80e7274c873da02
Bill Proudfoot <user@CODECLAN031.local>
2017-06-07 14:48:47 +0100

    working game

--- client/components/TictactoeSquare.jsx
+++ client/components/TictactoeSquare.jsx
@@ -2,18 +2,42 @@ import React from 'react';
 
 class TictactoeSquare extends React.Component {
 
-constructor(props) {
-  super(props)
+  constructor(props) {
+    super(props)
+    this.state={ buttonSymbol: this.props.position}
+  }
 
-}
 
   render () {
     return (
       <div>
-        <button>{this.props.position}</button>
+      <button onClick={ () => { this.handleClick() } }>{this.state.buttonSymbol}</button>
       </div>
-    );
+      );
   }
+
+  handleClick () {
+  // console.log("HandleClick", this.props.position)
+
+  if (this.state.buttonSymbol !== "X" && this.state.buttonSymbol !== "O"){
+
+    console.log("HandleClick", this.props.position)
+
+
+    if (this.props.crossesTurn) {
+      this.setState({buttonSymbol: "X"})
+    }
+
+    else {
+      this.setState({buttonSymbol: "O"})
+    }
+    this.props.recordMove(this.props.position);
+    this.props.changeTurn();
+
+    // console.log(this.onClick)
+  }
+}
+
 
 }
 
--- client/containers/TictactoeBoard.jsx
+++ client/containers/TictactoeBoard.jsx
@@ -5,45 +5,89 @@ class TictactoeBoard extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = { squares: [],
-      crossesTurn: true
-     }
+    this.state = { 
+      squares: [1,2,3,4,5,6,7,8,9],
+      crossesTurn: true,
+      winner: "Not Yet"
+    };
+    this.changeTurn = this.changeTurn.bind(this);
+    this.recordMove = this.recordMove.bind(this);
   }
+
+  changeTurn () {
+
+
+    this.setState((prevState) => { return { crossesTurn: !prevState.crossesTurn} })
+  }
+
+  recordMove (position) {
+    var squares = this.state.squares 
+    squares[position - 1 ] = this.state.crossesTurn? "X":"O" 
+    this.setState((prevState) => { return { squares: squares} })
+    this.winnerCheck()
+  }
+
+  winnerCheck() {
+    // Check rows for winners
+    for (let i=0; i < 9; i+=3) {
+      if (this.state.squares[i+0]===this.state.squares[i+1] && this.state.squares[i+1]===this.state.squares[i+2]) {
+        this.setState((prevState) => {return {winner: this.state.squares[i+0]}})
+      }
+    }
+    // Check columns for winners
+    for (let i=0; i < 3; i++) {
+      if (this.state.squares[0+i]===this.state.squares[3+i] && this.state.squares[3+i]===this.state.squares[6+i]) {
+        this.setState((prevState) => {return {winner: this.state.squares[0+i]}})
+      }
+    }
+    // Check diagonals for winners
+    if (this.state.squares[0]===this.state.squares[4] && this.state.squares[4]===this.state.squares[8]) {
+      this.setState((prevState) => {return {winner: this.state.squares[4]}})
+    }
+    if (this.state.squares[2]===this.state.squares[4] && this.state.squares[4]===this.state.squares[6]) {
+      this.setState((prevState) => {return {winner: this.state.squares[4]}})
+    }
+  }
+
 
   render () {
 
-     
 
-      var table = []
-      for (let i = 1; i < 10; i += 3 ){
 
-        var squares = [];
-        for (let k = 0; k < 3; k++ ){
-          squares.push(<td key={ k+ i}><TictactoeSquare position={ k + i } /></td>);
+    var table = []
+    for (let i = 1; i < 10; i += 3 ){
+
+      var squares = [];
+      for (let k = 0; k < 3; k++ ){
+        squares.push(<td key={ k+ i}><TictactoeSquare position={ k + i }  crossesTurn = { this.state.crossesTurn} changeTurn={this.changeTurn} recordMove={this.recordMove}/></td>);
         //  table.push(square)
-        }
-        table.push( <tr key={ i }>{squares}</tr> );
       }
-      
-      var crossesTurnText = ""
-      if (this.state.crossesTurn) {
-        crossesTurnText = " Yes "
-      }
-        else { crossesTurnText = " No "}
+      table.push( <tr key={ i }>{squares}</tr> );
+    }
 
-    return (
-      <div>
-      <h1> This is a Tic Tac Toe Board </h1>
+    var crossesTurnText = ""
+    if (this.state.crossesTurn) {
+      crossesTurnText = " Yes "
+    }
+    else { crossesTurnText = " No "}
 
-      <h3> Crosses Turn: {crossesTurnText} </h3>
-     
-      <table> 
-      <tbody>
-      { table }
-      </tbody>
-      </table>
-      </div>
-    );
+      return (
+        <div>
+        <h1> This is a Tic Tac Toe Board </h1>
+
+        <h3> Crosses Turn: {crossesTurnText} </h3>
+
+        <h3> Winner is: { this.state.winner}</h3>
+
+        <div className="center">
+        <table> 
+        <tbody>
+        { table }
+        </tbody>
+        </table>
+        </div>
+        </div>
+        );
   }
 
 }
