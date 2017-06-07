e6ee770321ea11b921106a277a7d06fbf7c65b3f
Bill Proudfoot <user@CODECLAN031.local>
2017-06-07 15:20:32 +0100

    stopped game after winner

--- client/components/TictactoeSquare.jsx
+++ client/components/TictactoeSquare.jsx
@@ -4,35 +4,38 @@ class TictactoeSquare extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state={ buttonSymbol: this.props.position}
+    // this.state={ buttonSymbol: this.props.position}
   }
 
 
   render () {
     return (
       <div>
-      <button onClick={ () => { this.handleClick() } }>{this.state.buttonSymbol}</button>
+      <button onClick={ () => { this.handleClick() } }>{this.props.squareValue}</button>
       </div>
       );
+
+    //      <button onClick={ () => { this.props.handleClick(this.props.buttonSymbol) } }>{this.state.buttonSymbol}</button>
   }
 
   handleClick () {
   // console.log("HandleClick", this.props.position)
 
-  if (this.state.buttonSymbol !== "X" && this.state.buttonSymbol !== "O"){
+  if (this.props.squareValue !== "X" && this.props.squareValue !== "O" && this.props.winner === "Not Yet"){
 
-    console.log("HandleClick", this.props.position)
+    this.props.squareClicked(this.props.position);
+    // console.log("HandleClick", this.props.position)
 
 
-    if (this.props.crossesTurn) {
-      this.setState({buttonSymbol: "X"})
-    }
+    // if (this.props.crossesTurn) {
+    //   this.setState({buttonSymbol: "X"})
+    // }
 
-    else {
-      this.setState({buttonSymbol: "O"})
-    }
-    this.props.recordMove(this.props.position);
-    this.props.changeTurn();
+    // else {
+    //   this.setState({buttonSymbol: "O"})
+    // }
+    // this.props.recordMove(this.props.position);
+    // this.props.changeTurn();
 
     // console.log(this.onClick)
   }
--- client/containers/TictactoeBoard.jsx
+++ client/containers/TictactoeBoard.jsx
@@ -5,13 +5,18 @@ class TictactoeBoard extends React.Component {
 
   constructor(props) {
     super(props)
+    let squares = []
+    for (let i=0; i <9; i++){
+      squares.push("")
+    }
     this.state = { 
-      squares: [1,2,3,4,5,6,7,8,9],
+      squares: squares,
       crossesTurn: true,
       winner: "Not Yet"
     };
     this.changeTurn = this.changeTurn.bind(this);
     this.recordMove = this.recordMove.bind(this);
+    this.squareClicked = this.squareClicked.bind(this);
   }
 
   changeTurn () {
@@ -30,36 +35,39 @@ class TictactoeBoard extends React.Component {
   winnerCheck() {
     // Check rows for winners
     for (let i=0; i < 9; i+=3) {
-      if (this.state.squares[i+0]===this.state.squares[i+1] && this.state.squares[i+1]===this.state.squares[i+2]) {
+      if (this.state.squares[i+0] !== "" && this.state.squares[i+0]===this.state.squares[i+1] && this.state.squares[i+1]===this.state.squares[i+2]) {
         this.setState((prevState) => {return {winner: this.state.squares[i+0]}})
       }
     }
     // Check columns for winners
     for (let i=0; i < 3; i++) {
-      if (this.state.squares[0+i]===this.state.squares[3+i] && this.state.squares[3+i]===this.state.squares[6+i]) {
+      if (this.state.squares[i+0] !== "" && this.state.squares[0+i]===this.state.squares[3+i] && this.state.squares[3+i]===this.state.squares[6+i]) {
         this.setState((prevState) => {return {winner: this.state.squares[0+i]}})
       }
     }
     // Check diagonals for winners
-    if (this.state.squares[0]===this.state.squares[4] && this.state.squares[4]===this.state.squares[8]) {
+    if (this.state.squares[4] !== "" && this.state.squares[0]===this.state.squares[4] && this.state.squares[4]===this.state.squares[8]) {
       this.setState((prevState) => {return {winner: this.state.squares[4]}})
     }
-    if (this.state.squares[2]===this.state.squares[4] && this.state.squares[4]===this.state.squares[6]) {
+    if (this.state.squares[4] !== "" && this.state.squares[2]===this.state.squares[4] && this.state.squares[4]===this.state.squares[6]) {
       this.setState((prevState) => {return {winner: this.state.squares[4]}})
     }
   }
 
+  squareClicked(position) {
+    this.recordMove(position);
+    this.changeTurn();
+  }
+
 
   render () {
-
-
 
     var table = []
     for (let i = 1; i < 10; i += 3 ){
 
       var squares = [];
       for (let k = 0; k < 3; k++ ){
-        squares.push(<td key={ k+ i}><TictactoeSquare position={ k + i }  crossesTurn = { this.state.crossesTurn} changeTurn={this.changeTurn} recordMove={this.recordMove}/></td>);
+        squares.push(<td key={ k+ i}><TictactoeSquare position={ k + i } squareClicked={this.squareClicked} squareValue={this.state.squares[(k+i)-1]} winner={this.state.winner}/></td>);
         //  table.push(square)
       }
       table.push( <tr key={ i }>{squares}</tr> );
